a7b64d4f607c6b34177c039e8ecf22d6cf12dd66
Meredith Salguero <salguero.meredith@gmail.com>
2015-11-04 15:25:31 -0500

    completed Act 2

--- main.js
+++ main.js
@@ -3,25 +3,29 @@ $(document).ready(function(){
 
 var $input = $('#prompt').val();
 var $list = $('#log');
-bank = 10000;
+var bank = 10000;
+
 
 var addToList = function(){
 	var $input = $('#prompt').val();
 		if(event.which===13){
-			if($input==="purse"){
-				getPurse();
-			}else{
+			var $stringToArray = $input.split(' ');
 			var $createLi = $('<li>');
+			$createLi.attr('class', 'command');
 			$createLi.text($input);
 			$list.append($createLi);
+			if($input==="purse"){
+				getPurse();
+			}else if($stringToArray[0]==='loan'){
+				lendMoney();
+			}
 			$('#prompt').val('');
-		}
 	}	
 };
 
 $(document).keypress(addToList);
 
-// if($input==="purse"){
+
 var getPurse = function(){
  var $createLi = $('<li>');
  	$createLi.attr('class', 'action');
@@ -30,7 +34,21 @@ var getPurse = function(){
 	console.log($createLi);
 	$('#prompt').val('');
 }
-// }
+
+var lendMoney = function(){	
+	var $input = $('#prompt').val();
+	var $stringToArray = $input.split(' ');
+	var $amount = $input.replace ( /[^\d.]/g, '' );
+	console.log($amount);
+	bank += parseInt($amount);
+	var $createLi = $('<li>');
+ 	$createLi.attr('class', 'action');
+	$createLi.text('ShylockBot gives '+ $stringToArray[1]+ " "+ $amount + ' ducats');
+	$list.append($createLi);
+	console.log(bank);
+	$('#prompt').val('');
+
+}
 
 
 });
